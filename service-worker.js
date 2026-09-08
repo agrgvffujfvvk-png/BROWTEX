8db304925a73bc5cfd4242c78720ac6b4b1c6b31
agrgvffujfvvk-png <agrgvffujfvvk@gmail.com>
2026-09-08 20:03:35 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = "tixit-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./search.html",
+  "./library.html",
+  "./add.html",
+  "./viewer.html",
+  "./settings.html",
+  "./css/style.css",
+  "./js/database.js",
+  "./js/app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type === "opaque"
+          ) {
+            return response;
+          }
+
+          const responseClone = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
+    })
+  );
+});
